5838cfdb5682676fc76e401cb3be475f4999c9e2
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-06-01 20:53:07 -0500

    Superiorwalls: Update dependencies.
    -  Add muzei api as dependency
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: I338379d328c8c065e74e8ac0c746b5fb208c936a

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused")
 
 object Frames {
-    const val appId = "com.superior.superiorwalls"
+    const val appId = "com.sipun.superiorwalls"
     const val version = 201
     const val versionName = "2.0.1"
 }
