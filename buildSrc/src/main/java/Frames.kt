667a07023d31d90dbeab9f8c7ea663534288ecf4
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-04-11 23:44:33 +0530

    Superiorwalls: Bump version to 2.0.5
    - Updated dependencies.
    - Update code to not use deprecated variants
    - Fix crash when applying with external apps
    
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: If46c1f02c91b3f0b3a8ade7966c6bc4d1ba55543

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -2,6 +2,6 @@
 
 object Frames {
     const val appId = "com.sipun.superiorwalls"
-    const val version = 204
-    const val versionName = "2.0.4"
+    const val version = 205
+    const val versionName = "2.0.5"
 }
