708a68246ee8eac89a57d1475dd3612285683563
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-05-02 18:02:02 -0500

    Superiorwalls: Fix content not appearing on first load
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: If1bb3cfad0d027a3541b4bd43d1e7700742f56c6

--- library/src/main/kotlin/dev/jahir/frames/data/viewmodels/WallpapersDataViewModel.kt
+++ library/src/main/kotlin/dev/jahir/frames/data/viewmodels/WallpapersDataViewModel.kt
@@ -234,11 +234,12 @@ open class WallpapersDataViewModel(application: Application) : AndroidViewModel(
         url: String = "",
         loadCollections: Boolean = true,
         loadFavorites: Boolean = true,
-        force: Boolean = false
+        force: Boolean = false,
+        triggerErrorListener: Boolean = true,
     ) {
         if (!url.hasContent()) return
         if (!context.isNetworkAvailable()) {
-            errorListener?.invoke(DataError.NoNetwork)
+            if (triggerErrorListener) errorListener?.invoke(DataError.NoNetwork)
             handleWallpapersData()
             return
         }
@@ -246,7 +247,7 @@ open class WallpapersDataViewModel(application: Application) : AndroidViewModel(
             val remoteWallpapers = service.getJSON(url)
             handleWallpapersData(loadCollections, loadFavorites, remoteWallpapers, force)
         } catch (e: Exception) {
-            errorListener?.invoke(DataError.MalformedJson)
+            if (triggerErrorListener) errorListener?.invoke(DataError.MalformedJson)
         }
     }
 
@@ -254,12 +255,13 @@ open class WallpapersDataViewModel(application: Application) : AndroidViewModel(
         url: String = "",
         loadCollections: Boolean = true,
         loadFavorites: Boolean = true,
-        force: Boolean = false
+        force: Boolean = false,
+        triggerErrorListener: Boolean = true,
     ) {
         viewModelScope.launch {
             delay(10)
             handleWallpapersData(loadCollections, loadFavorites, listOf(), force, true)
-            loadRemoteData(url, loadCollections, loadFavorites, force)
+            loadRemoteData(url, loadCollections, loadFavorites, force, triggerErrorListener)
         }
     }
 
