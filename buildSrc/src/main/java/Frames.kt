3be427cc515dfed54bc112f9f8aa34d6499c614c
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-07-29 10:40:47 -0500

    Superiorwalls: update version and dependencies
    Change-Id: Iec43cf83d76d20f49d29835fa1c5789f6ecb43d2
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -2,6 +2,6 @@
 
 object Frames {
     const val appId = "com.sipun.superiorwalls"
-    const val version = 331
-    const val versionName = "3.3.1"
+    const val version = 332
+    const val versionName = "3.3.2"
 }
