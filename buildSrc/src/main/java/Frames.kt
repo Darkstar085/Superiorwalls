3895b699e6d2a8faebafde5b74637b530c92ec1a
S I P U N <sipunkumar85@gmail.com>
2025-07-05 16:15:10 +0530

    Superiorwalls: Update version
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- buildSrc/src/main/java/Frames.kt
+++ buildSrc/src/main/java/Frames.kt
@@ -2,6 +2,6 @@
 
 object Frames {
     const val appId = "com.sipun.superiorwalls"
-    const val version = 333
-    const val versionName = "3.3.3"
+    const val version = 334
+    const val versionName = "3.3.4"
 }
