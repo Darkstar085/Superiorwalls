9e166ba8a8055c65edfa8c4ed46db2d892384f17
S I P U N <sipunkumar85@gmail.com>
2026-09-16 12:50:13 +0530

    chore: bump app version to 1.0
    - Set versionName to 1.0
    - Increment versionCode to 6
    
    Change-Id: I8f3c6a1d9b4e7f2c5a0d8e6b3f1c9a7d4e2b5f8c
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sipun.superiorwalls"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "1.0"
     }
 
     signingConfigs {
