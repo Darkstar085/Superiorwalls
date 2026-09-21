a6dfb9cc9ab57d25ec71eb0a24b13f8387feabf6
S I P U N <sipunkumar85@gmail.com>
2026-09-22 00:17:09 +0530

    chore: bump app version to 1.5
    - Update the app version name and version code.
    
    Change-Id: I5a1c7e9d3b6f2048c0a7e5d1b9f3c6a8e2d4b7c1
    Signed-off-by: S I P U N <sipunkumar85@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sipun.superiorwalls"
         minSdk = 29
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.0"
+        versionCode = 7
+        versionName = "1.5"
     }
 
     signingConfigs {
